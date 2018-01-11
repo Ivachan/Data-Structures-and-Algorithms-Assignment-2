be4eaf0c201f9d21c10a7f1bee94eeb0711e8e3e
Ivachan <32516596+Ivachan@users.noreply.github.com>
2018-01-11 20:00:22 +0200

    Add files via upload

---
+++ DataStructures.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_and_Algorithms_Assignment_1
+{
+    class DataStructures
+    {
+        private int legth; 
+        private int first;
+        private int last;
+
+        private List<int> listOFInt = new List<int>();
+        public List<int> ListOfInt()
+        {
+            return listOFInt.ToList();
+        }
+
+        public void addElements(int N)
+        {
+            listOFInt.Add(N);
+        }
+
+        public int countLength()
+        {
+            legth = listOFInt.Count;
+            if (listOFInt != null)
+            {
+                return legth;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private int firstElment()
+        {
+            first = listOFInt.ElementAt(0);
+            if (listOFInt != null)
+            {
+                return first;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private int lastElement()
+        {
+            last = listOFInt.ElementAt(2);
+            if (listOFInt != null)
+            {
+                return last;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
+        public int getElement(int index)
+        {
+            index = listOFInt.ElementAt(index);
+
+            if (listOFInt != null)
+            {
+                return index;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void removeIndex(int index)
+        {
+            if (listOFInt != null)
+            {
+                listOFInt.RemoveAt(index);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        public void removeFirst(int first)
+        {
+            if (listOFInt != null)
+            {
+                listOFInt.RemoveAt(first);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        public void removeLast(int last)
+        {
+            if (listOFInt != null)
+            {
+                listOFInt.RemoveAt(last);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        public int getFirst()
+        {
+            return firstElment();
+        }
+
+        public int getLast()
+        {
+            return lastElement();
+        }
+
+        public void sort()
+        {
+            try
+            {
+                listOFInt = listOFInt.OrderBy(q => q).ToList();
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        public int getMaxElement()
+        {
+            return getLast();
+        }
+
+        public int getMinElement()
+        {
+            return getFirst();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_and_Algorithms_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataStructures listOfNumbers()
+            {
+                DataStructures myDATAlist = new DataStructures();
+                myDATAlist.addElements(4);
+                myDATAlist.addElements(19);
+                myDATAlist.addElements(89);
+                myDATAlist.addElements(44);
+                myDATAlist.addElements(25);
+                myDATAlist.addElements(32);
+                return myDATAlist;
+            }
+
+            Console.WriteLine(new string('^', 25));
+            Console.WriteLine("List of numbers: ");
+            DataStructures list = listOfNumbers();
+            foreach (int d in list.ListOfInt())
+            {
+                Console.WriteLine(d);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(new string('^', 25));
+            Console.WriteLine("Remove first element: ");
+            list.removeFirst(0);
+            foreach (int f in list.ListOfInt())
+            {
+                Console.WriteLine(f);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(new string('^', 25));
+            Console.WriteLine("Remove last element: ");
+            list.removeLast(4);
+            foreach (int l in list.ListOfInt())
+            {
+                Console.WriteLine(l);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(new string('^', 25));
+            Console.WriteLine("Remove some element: ");
+            list.removeIndex(2);
+            foreach (int i in list.ListOfInt())
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(new string('^', 25));
+            Console.WriteLine("First element: " + list.getFirst());
+            Console.WriteLine();
+            Console.WriteLine("Last element: " + list.getLast());
+            Console.WriteLine();
+
+            Console.WriteLine(new string('^', 25));
+            Console.WriteLine("Sorted list:");
+            list.sort();
+            foreach (int o in list.ListOfInt())
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(new string('^', 25));
+            Console.WriteLine("Max element: " + list.getMaxElement());
+            Console.WriteLine();
+            Console.WriteLine("Min element: " + list.getMinElement());
+            Console.WriteLine();
+
+            Console.WriteLine(new string('^', 25));
+            Console.WriteLine("Numbers in list: " + list.countLength());
+            Console.WriteLine();
+        }
+    }
+}
+    
+
